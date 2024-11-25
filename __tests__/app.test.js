06b278eb0f76522914d36b6daeb752da2e2733ef
kaleemxshraf <148862754+kaleemxshraf@users.noreply.github.com>
2024-11-25 15:39:04 +0000

    add GET/api/articles by specific id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -20,30 +20,68 @@ describe("GET /api", () => {
         expect(endpoints).toEqual(endpointsJson);
       });
   });
-});
 
-describe("GET /api/topics", () => {
-  test("200: should return an array of topics objects when topics exist", () => {
-    return request(app)
-      .get("/api/topics")
-      .expect(200)
-      .then(({ body: { topics } }) => {
-        expect(topics).toHaveLength(data.topicData.length);
-        topics.forEach((topic) => {
-          expect(topic).toMatchObject({
-            description: expect.any(String),
-            slug: expect.any(String),
+  describe("GET /api/topics", () => {
+    test("200: should return an array of topics objects when topics exist", () => {
+      return request(app)
+        .get("/api/topics")
+        .expect(200)
+        .then(({ body: { topics } }) => {
+          expect(topics).toHaveLength(data.topicData.length);
+          topics.forEach((topic) => {
+            expect(topic).toMatchObject({
+              description: expect.any(String),
+              slug: expect.any(String),
+            });
           });
         });
-      });
+    });
+
+    test("404: should respond with an error message if endpoint is invalid or misspelled", () => {
+      return request(app)
+        .get("/api/topicss")
+        .expect(404)
+        .then(({ body: { msg } }) => {
+          expect(msg).toBe("Not Found");
+        });
+    });
   });
 
-  test("400: should respond with an error message if endpoint is invalid", () => {
-    return request(app)
-      .get("/api/topicss")
-      .expect(400)
-      .then(({ body }) => {
-        expect(body.msg).toBe("Route Not Found");
+  describe("GET /api/articles", () => {
+    describe("GET /api/articles/:article_id", () => {
+      test("200 - returns the correct article object when valid article_id provided", () => {
+        return request(app)
+          .get("/api/articles/2")
+          .expect(200)
+          .then(({ body: { article } }) => {
+            expect(article).toMatchObject({
+              article_id: 2,
+              title: expect.any(String),
+              topic: expect.any(String),
+              author: expect.any(String),
+              body: expect.any(String),
+              created_at: expect.any(String),
+              votes: expect.any(Number),
+              article_img_url: expect.any(String),
+            });
+          });
       });
+      test("400 sends an appropriate status and error message when given an invalid id", () => {
+        return request(app)
+          .get("/api/articles/invalidId")
+          .expect(400)
+          .then(({ body: { msg } }) => {
+            expect(msg).toBe("Bad request");
+          });
+      });
+      test("404: should return a 404 and error message if no topics are found with the specific id", () => {
+        return request(app)
+          .get("/api/articles/999")
+          .expect(404)
+          .then(({ body: { msg } }) => {
+            expect(msg).toBe("Article Not Found");
+          });
+      });
+    });
   });
 });
--- models/app.model.js
+++ models/app.model.js
@@ -5,3 +5,17 @@ exports.fetchTopics = () => {
     return rows;
   });
 };
+
+exports.fetchArticles = (article_id) => {
+  return db
+    .query(`SELECT * FROM articles WHERE article_id= $1`, [article_id])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: "Article Not Found",
+        });
+      }
+      return rows[0];
+    });
+};
