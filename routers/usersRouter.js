89036272d2195eadc111c4e81be1bfd94edcf70b
kaleemxshraf <148862754+kaleemxshraf@users.noreply.github.com>
2024-11-29 16:45:56 +0000

    add route get users/:username

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getUsers } = require("../controllers/app.controller");
+const {
+  getUsers,
+  getUsersByUsername,
+} = require("../controllers/app.controller");
 
 router.get("/", getUsers);
+router.get("/:username", getUsersByUsername);
 
 module.exports = router;
