ff7bf7852e61c8c768b0c14424adf43136ed231f
kaleemxshraf <148862754+kaleemxshraf@users.noreply.github.com>
2024-11-29 12:15:59 +0000

    add express routes

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 const express = require("express");
-const {
-  getEndpoints,
-  getTopics,
-  getArticlesById,
-  getArticles,
-  getArticleComments,
-  postComment,
-  updateArticleVotes,
-  deleteComment,
-  getUsers,
-} = require("./controllers/app.controller");
+const app = express();
+const apiRouter = require("./routers/apiRouter");
+
 const {
   customErrorHandler,
   serverErrorHandler,
@@ -17,34 +9,16 @@ const {
   psqlErrorHandler,
 } = require("./errors");
 
-const app = express();
-
 app.use(express.json());
 
-app.get("/api", getEndpoints);
+app.use("/api", apiRouter);
 
-app.get("/api/topics", getTopics);
-
-app.get("/api/users", getUsers);
-
-app.get("/api/articles", getArticles);
-
-app.get("/api/articles/:article_id", getArticlesById);
-
-app.get("/api/articles/:article_id/comments", getArticleComments);
-
-app.post("/api/articles/:article_id/comments", postComment);
-
-app.patch("/api/articles/:article_id", updateArticleVotes);
-
-app.delete("/api/comments/:comment_id", deleteComment);
-
+// Handle all other undefined routes
 app.all("*", allEndpointErrorHandler);
 
+// Error Handlers
 app.use(psqlErrorHandler);
-
 app.use(customErrorHandler);
-
 app.use(serverErrorHandler);
 
 module.exports = app;
---
+++ routers/apiRouter.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const apiRouter = express.Router();
+
+const articlesRouter = require("./articlesRouter");
+const commentsRouter = require("./commentsRouter");
+const usersRouter = require("./usersRouter");
+const topicsRouter = require("./topicsRouter");
+
+// API route for Endpoints docs
+const { getEndpoints } = require("../controllers/app.controller");
+apiRouter.get("/", getEndpoints);
+
+apiRouter.use("/articles", articlesRouter);
+apiRouter.use("/comments", commentsRouter);
+apiRouter.use("/users", usersRouter);
+apiRouter.use("/topics", topicsRouter);
+
+module.exports = apiRouter;
