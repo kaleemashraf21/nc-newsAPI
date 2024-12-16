6967b9799f904050d8ba5a630772e8b67dd9fbc3
kaleemxshraf <148862754+kaleemxshraf@users.noreply.github.com>
2024-12-16 09:47:46 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const apiRouter = require("./routers/apiRouter");
+const cors = require("cors");
+app.use(cors());
 
 const {
   customErrorHandler,
