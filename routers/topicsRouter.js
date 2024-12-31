20fb1b4c74194d3ba3300b186bc81064df00d46d
kaleemxshraf <148862754+kaleemxshraf@users.noreply.github.com>
2024-12-31 16:36:46 +0000

    update routes

--- routers/topicsRouter.js
+++ routers/topicsRouter.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { getTopics } = require("../controllers/app.controller");
+const { getTopics, postTopic } = require("../controllers/app.controller");
 
 router.get("/", getTopics);
+router.post("/", postTopic);
 
 module.exports = router;
